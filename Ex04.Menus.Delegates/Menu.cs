f8e24c4feed433b158f7e32a5e61eaa5b3c7238d
gideonsh <58482256+gideonsh@users.noreply.github.com>
2020-06-15 17:08:21 +0300

    ddd

---
+++ Ex04.Menus.Delegates/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex04.Menus.Delegates
+{
+    class Menu
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Ex04.Menus.Interfaces/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex04.Menus.Interfaces
+{
+    class Menu
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
